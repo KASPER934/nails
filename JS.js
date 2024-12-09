476ffba85116828984c292d003cea7a0db67264d
KASPER934 <133381963+KASPER934@users.noreply.github.com>
2024-12-10 00:04:50 +0100

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+    mousewhell: true,
+    keyboard: true,
+    loop: true,
+  slidesPerView: 2.5,
+  spaceBetween: 30,
+  centeredSlides: true,
+});
+
